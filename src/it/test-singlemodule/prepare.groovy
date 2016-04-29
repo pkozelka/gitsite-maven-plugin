9c17e1104e86664b7afbc11f1f0f8d97e72ea282
Petr Kozelka <pkozelka@gmail.com>
2016-04-29 22:22:43 +0200

    added first integration test

---
+++ src/it/test-singlemodule/prepare.groovy
@@ -0,0 +1,4 @@
+import net.kozelka.utils.IntegrationTestUtils
+
+IntegrationTestUtils.createGitRepo(new File(basedir, "target/gitsiterepo"))
+assert true
---
+++ src/it/test-singlemodule/verify.groovy
@@ -0,0 +1,2 @@
+//todo check result
+assert true
